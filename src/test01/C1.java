71f9b043068a168d199cb4952161d760b2443a1a
Sena Yucedal <sena.ycdl61@gmail.com>
2022-09-08 23:49:38 +0300

    sena ikinci commit

--- src/test01/C1.java
+++ src/test01/C1.java
@@ -3,5 +3,10 @@ package test01;
 public class C1 {
     public static void main(String[] args) {
         System.out.println("merhaba");
+
+        System.out.println("Merhaba dunya");
+
     }
+
+
 }
